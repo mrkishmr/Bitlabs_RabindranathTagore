961e918290e225a583c897eea982408a9284048f
M R Kishan <106878140+mrkishmr@users.noreply.github.com>
2023-05-02 17:15:18 +0530

    Add files via upload

---
+++ BitLabs_Week3/Bitlabs_Programs/Program1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Prog1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter the values of a and b");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println("The value of a+b is " + (a+b));
+	}
+}
